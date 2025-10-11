2a7811bd6dcdddc5b5f4e9af6587d4f6e6fdfe77
Pyn Chaingin <stevie@Pyns-Macbook-Pro.local>
2025-10-11 11:06:21 +0700

    Fix login page

--- express/index.js
+++ express/index.js
@@ -2,28 +2,60 @@ import express from "express"
 import fetch from "node-fetch"
 import path from "path"
 import {fileURLToPath} from "url"
+import axios from "axios"
+import session from "express-session"
+import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const DJANGO_URL = "http://127.0.0.1:8000"
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/product')
+  }
+  res.render('login')
+})
+
 app.get('/login', async (req, res) => {
+  const {username, password} = req.body;
+
+  try {
+    const tokenRes = await axios.post(`${DJANGO_URL}/user/token/`, {username, password})
+    const token = tokenRes.data.access
+
+    res.redirect('/product')
+    
+  }
+  catch (err) {
+    res.send("Bad request")
+  }
+
 })
 
 app.get('/product', async (req, res) => {
+  if(!req.session.user){
+    res.redirect('/login')
+  }
   try {
-    const product_list = fetch("http://127.0.0.1:8000/product/")
-    const catogory_list = fetch("http://127.0.0.1:8000/category/")
+    const product_list = fetch(`${DJANGO_URL}/product/`)
+    const catogory_list = fetch(`${DJANGO_URL}/category/`)
 
   }
   catch (err) {
     res.status(500).json(`Error! : ${err}`);
     
   }
+})
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => res.redirect('/login'))
 })
 
 app.listen(port, () => {
--- store/app/urls.py
+++ store/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from app import views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path('product/', views.ListProduct.as_view()),
     path('category/', views.ListOfCategories.as_view()),
@@ -8,5 +10,9 @@ urlpatterns = [
     path('add_purchase_item/', views.AddPurchaseItem.as_view()),
     path('create_purchase/', views.CreatePurchase.as_view()),
     path('cancel_bill/<int:id>', views.CancelBill.as_view()),
+
+    #Authentication
+    path('user/token/refresh/', TokenRefreshView.as_view()),
+    path('user/token/', TokenObtainPairView.as_view()),
 
 ]
